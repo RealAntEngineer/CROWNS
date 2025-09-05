cd9ea4609dae081759cfd124a681196186cae272
Real Ant Engineer <lafourmiedu35@gmail.com>
2025-09-05 18:53:00 +0200

    sync with 1.21.1

--- src/main/java/com/rae/crowns/init/misc/FluidInit.java
+++ src/main/java/com/rae/crowns/init/misc/FluidInit.java
@@ -9,6 +9,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
 import net.minecraftforge.fluids.FluidType;
+import net.minecraftforge.fluids.ForgeFlowingFluid;
 import net.minecraftforge.registries.DeferredRegister;
 
 
@@ -35,7 +36,19 @@ public class FluidInit {
                     .build()
                     .register();
 
-
+    public static final FluidEntry<ForgeFlowingFluid.Flowing> URANIUM_HEXAFLUOR =
+            CROWNS.REGISTRATE.fluid("uranium_hexafluor" ,CROWNS.resource("fluid/uranium_hexafluoride_still"), CROWNS.resource("fluid/uranium_hexafluoride_flowing"))
+                    .lang("URANIUM_HEXAFLUOR")
+                    .properties(b -> b.viscosity(2000)
+                            .density(1400))
+                    .fluidProperties(p -> p.levelDecreasePerBlock(2)
+                            .tickRate(25)
+                            .slopeFindDistance(3)
+                            .explosionResistance(100f))
+                    .source(ForgeFlowingFluid.Source::new) // TODO: remove when Registrate fixes FluidBuilder
+                    .bucket()
+                    .build()
+                    .register();
 
     private static FluidType defaultFluidType(FluidType.Properties properties, ResourceLocation stillTexture, ResourceLocation flowingTexture) {
         return new FluidType(properties) {
