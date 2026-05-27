c73d919c7ed5b976af36988b46f12dfc6ebe475e
rgjava <rgjava10@gmail.com>
2026-05-27 11:58:26 -0600

    WIP before switching to 1.21.1-dev.

---
+++ src/main/java/com/rae/crowns/content/nuclear/detectors/NeutronDetector.java
@@ -0,0 +1,12 @@
+package com.rae.crowns.content.nuclear.detectors;
+
+import net.minecraft.world.level.block.Block;
+import org.jetbrains.annotations.NotNull;
+
+public class NeutronDetector extends Block {
+
+    public NeutronDetector(@NotNull Properties properties) {
+        super(properties);
+    }
+
+}
