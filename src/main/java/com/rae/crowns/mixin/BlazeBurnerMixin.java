851297c842557d333f95d41d695a4840fafcb4cc
Real Ant Engineer <lafourmiedu35@gmail.com>
2026-06-03 14:55:37 +0200

    fixed a crash with blaze burner

--- src/main/java/com/rae/crowns/mixin/BlazeBurnerMixin.java
+++ src/main/java/com/rae/crowns/mixin/BlazeBurnerMixin.java
@@ -28,11 +28,11 @@ public abstract class BlazeBurnerMixin extends SmartBlockEntity implements IHave
     @Override
     public float getTemperature() {
         return switch (getHeatLevelFromBlock()) {
-            case NONE -> 300f;
             case SMOULDERING -> 500F;
             case FADING -> 600F;
             case KINDLED -> 1200F;
             case SEETHING -> 1600F;
+            default -> 300f;
         };
     }
 
